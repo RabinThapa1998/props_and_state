07159947282772a8da1d8cd27b75d4288a46a9be
RabinThapa1998 <imrabin1998@gamil.com>
2022-08-09 19:48:58 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./styles/card.css";
+// import json data here
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Props and Maps</h1>
+      {/* add title component here */}
+      {/* map json data for each item and return card component here */}
     </div>
   );
 }
