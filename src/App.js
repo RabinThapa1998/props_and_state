efa398ce30d941e40c51f87cfc07daa5314fd01c
cecil <cecilghimire@gmail.com>
2022-08-12 19:29:30 +0545

    correction  applied for multiple render

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import './app.css'
 import { useEffect , useState} from 'react'
-import { Card } from "./components/UI/card";
 import { Todoslist } from './Context/data.js/context';
 import Mainsection from './components/sections/mainSection';
 function App() {
@@ -26,17 +25,13 @@ function App() {
   }, []);
   
 
-const todos=data.map((item)=>{
-     return <Card key={item.id} item={item}></Card>
-
-  })
   return (
     <Todoslist.Provider value={data}>
     <div className='app'>
       <center><h1>Todos List</h1>
       {/* add title component here */}
       {/* map json data for each item and return card component here */}
-      {todos}
+   
      <Mainsection></Mainsection>
       </center>
     </div>
