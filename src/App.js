546307cacd26147d44d3cb7e7f7ee124a15e9964
cecil <cecilghimire@gmail.com>
2022-08-12 18:42:51 +0545

    add map and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import "./styles/card.css";
+
 // import json data here
+import { useEffect , useState} from 'react'
+import { Card } from "./components/UI/card";
+import './app.css'
 function App() {
+  
+  const [data, setData] = useState([]);
+  async function renderData() {
+    try {
+      const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+      const data = await response.json();
+      console.log(data);
+      setData(data);
+    }
+      catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    renderData();
+  }, []);
+  
+
+const todos=data.map((item)=>{
+     return <Card key={item.id} item={item}></Card>
+
+  })
   return (
-    <div>
-      <h1>Props and Maps</h1>
+    <div className='app'>
+      <center><h1>Todos List</h1>
       {/* add title component here */}
       {/* map json data for each item and return card component here */}
+      {todos}
+      </center>
     </div>
   );
 }
---
+++ src/components/UI/card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../styles/card.css'
+export const Card = ({item}) => {
+  return (
+    <div className='card-container'>
+        
+        <h1 className='number'>{item.id}</h1>
+        <h2 className='tiltle'>{item.title}</h2>
+        { item.completed?<span className='completed'><h6>completed</h6></span>:<span className='not-completed'><h6>not-completed</h6></span>}
+    </div>
+  )
+}
